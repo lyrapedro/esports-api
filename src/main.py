6399bd43ff704beebdfa9def9e939d409dc7b922
Pedro Lyra <pedrolyradev@outlook.com>
2025-04-13 15:40:24 -0300

    Update main.py to correct port

--- src/main.py
+++ src/main.py
@@ -28,4 +28,4 @@ def root():
     return RedirectResponse(url="/docs")
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=3001)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080)
