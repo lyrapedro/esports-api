1358ad68077bc5b2b783010961658969b380ca93
Pedro Lyra <pedrolyradev@outlook.com>
2025-04-13 22:50:37 -0300

    feat: getting live matches from event

--- src/core/scrapers/vlrgg/matches.py
+++ src/core/scrapers/vlrgg/matches.py
@@ -2,59 +2,50 @@ import re
 from datetime import datetime, timezone
 from bs4 import BeautifulSoup
 import time
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 import requests
 
 headers = {
     "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0",
 }
 
-chrome_options = Options()
-chrome_options.add_argument("--headless")  # Executar sem abrir janela
-chrome_options.add_argument("--disable-gpu")  # Melhora performance em alguns sistemas
-chrome_options.add_argument("--window-size=1920x1080")  # Resolução padrão
-
 async def get_live_matches(event_url):
     """Get live matches for an event"""
 
-    driver = webdriver.Chrome(options=chrome_options)
-    driver.get(f"https://www.vlr.gg/event/matches{event_url}")
+    url = f'https://www.vlr.gg/event/matches{event_url}'
 
-    time.sleep(3)
-    content = driver.page_source
-
-    driver.quit()
+    content = requests.get(url).text
 
     bs = BeautifulSoup(content, 'html.parser')
-    live_matches = bs.select('a.match-item:has(div.mod-live)')
+
+    live_matches = []
+    for a_tag in bs.find_all('a', class_='wf-module-item'):
+        ml_status = a_tag.find('div', class_='ml-status')
+        if ml_status and ml_status.get_text(strip=True) == 'LIVE':
+            live_matches.append(a_tag)
 
     result = []
 
     for match in live_matches:
         teams_div = match.find('div', class_='match-item-vs')
         teams_names = teams_div.find_all('div', 'match-item-vs-team-name')
-        team1_name = teams_names[0].find('div', class_='text-of').text #
-        print(team1_name)
-        team2_name = teams_names[0].find('div', class_='text-of').text #
-        print(team2_name)
+        team1_name = teams_names[0].find('div', class_='text-of').text.strip()
+        team2_name = teams_names[1].find('div', class_='text-of').text.strip()
         match_url = match['href']
-        match_page = requests.get(f"https://www.vlr.gg/{match_url}").text
+        match_page = requests.get(f"https://www.vlr.gg{match_url}").text
         bs_match = BeautifulSoup(match_page, 'html.parser')
-        match_type = bs_match.select_one('.match-header-vs-note') #
-        print(match_type)
+        match_type = bs_match.select_one('.match-header-vs-note').text.strip()
         map_score = bs_match.select_one('.match-header-vs-score').find_all('span')
-        team1_map_score = map_score[0].text #
-        print(team1_map_score)
-        team2_map_score = map_score[1].text #
-        maps_containers = bs_match.find_all('div', class_='vm-stats-game ', attrs={'data-game-id': True})
+        team1_map_score = map_score[0].text.strip()
+        team2_map_score = map_score[2].text.strip() # 2 because index 1 is the separator (:)
+        maps_containers = bs_match.find_all('div', class_='vm-stats-game', attrs={'data-game-id': True})
+        map_divs = [div for div in maps_containers if div['data-game-id'] != 'all']
 
         maps = []
 
-        for map_container in maps_containers:
-            map_name = map_container.find('div', class_='map').select_one('span') #
-            team1_score = map_container.find_all('div', class_='team')[0].select_one('div').text #
-            team2_score = map_container.find_all('div', class_='team')[1].select_one('div').text #
+        for map_div in map_divs:
+            map_name = map_div.find('div', class_='map').select_one('span').find(text=True, recursive=False).strip() #only the inner text
+            team1_score = map_div.find_all('div', class_='team')[0].select_one('.score').text.strip()
+            team2_score = map_div.find_all('div', class_='team')[1].select_one('.score').text.strip()
 
             maps.append({
                 map_name: {
@@ -70,9 +61,7 @@ async def get_live_matches(event_url):
                 team1_name: team1_map_score,
                 team2_name: team2_map_score
             },
-            "maps": {
-                maps
-            }
+            "maps": maps
         })
 
     return result
